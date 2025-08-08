ff8f773ac5c9f6cdf1652b186edf9737c2461a5f
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2025-08-08 18:45:46 +0530

    Update .gitignore and axios configuration for environment variables
    - Added .env to .gitignore to prevent sensitive information from being tracked
    - Updated Axios instance baseURL to use environment variable for API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // matches backend routes like /api/auth
+  baseURL: "import.meta.env.VITE_API_URL",
   headers: {
     "Content-Type": "application/json",
   },
