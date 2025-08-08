409013fd99c3147b222f51c68fbc9e0863e57032
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2025-08-08 18:54:07 +0530

    .

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "import.meta.env.VITE_API_URL",
+  baseURL: "https://uptime-monitor-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
